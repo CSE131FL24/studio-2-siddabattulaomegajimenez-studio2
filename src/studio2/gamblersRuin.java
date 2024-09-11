78ebb46640914d4568175eb158fdc788f2fa788a
riyasiddabattula <riyasiddabattula@localhost>
2024-09-11 14:21:56 -0500

    unfinihsed stuiod 2

--- src/studio2/gamblersRuin.java
+++ src/studio2/gamblersRuin.java
@@ -12,9 +12,37 @@ Scanner in = new Scanner(System.in);
         System.out.print("Enter a starting amount: ");
         double startAmount = in.nextDouble();
         
+        System.out.print("Enter a win limit: "); 
+        int winLimit = in.nextInt();
         
-
+        System.out.print("Enter a win chance: ");
+        int winChance = in.nextInt();
+        
+        
+        double probability = (double) (Math.random()); 
+        
+        while (startAmount > 0 || startAmount < winLimit) {
+        	if (probability <= winChance) {
+        	startAmount = startAmount + 1;  }
+        	else {
+        		startAmount = startAmount - 1; 
+        	}
+       /* 	if (startAmount = winLimit) { 
+        		System.out.print("Success")
+        	if (startAmount = 0) {
+        			System.out.print("Ruin"); */
+        			
+       
+        		}
+        	}
+        	
+     
+        
+        
+      
+        		
+        
 	}
 
-}
+
 
