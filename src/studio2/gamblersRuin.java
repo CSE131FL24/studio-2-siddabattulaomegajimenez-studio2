bc3363b1b626fc58096132de65ed5b1cf2561776
riyasiddabattula <riyasiddabattula@localhost>
2024-09-11 14:18:10 -0500

    unfinished studio 2

---
+++ src/studio2/gamblersRuin.java
@@ -0,0 +1,20 @@
+package studio2;
+
+import java.util.Scanner;
+
+public class gamblersRuin {
+
+	public static void main(String[] args) {
+		
+Scanner in = new Scanner(System.in);
+
+        
+        System.out.print("Enter a starting amount: ");
+        double startAmount = in.nextDouble();
+        
+        
+
+	}
+
+}
+
